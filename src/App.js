5ffec1117e9d8c52d9d617545e231ba9ca1d844f
Hill Onyechekwa <hillonyechekwa@gmail.com>
2021-05-12 12:23:55 +0100

    removed unused imports in app.js

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@ import React from "react";
 import CharacterSearch from './CharacterSearch'
 import Details from './Details'
 import Home from './Home'
-import Logo from './Assets/logo.png';
-import {Router, Link} from '@reach/router'
 import "./styles.css";
 
 export default function App() {
